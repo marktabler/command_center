c099cfd02fd91486e0dab82d6e53d0ed6dedab72
Mark Tabler <mark.tabler@fallingmanstudios.net>
2012-06-28 13:26:29 -0400

    Fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 CommandCenter::Application.routes.draw do
   match '/auth/facebook/callback', to: "sessions#create"
-  match '/tos', to: 'pages#tos' as: :tos
+  match '/tos', to: 'pages#tos', as: :tos
   match '/privacy', to: 'pages#privacy', as: :privacy
   root :to => 'pages#home'
   resources :instructions
