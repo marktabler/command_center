59fadcf6323d55a5a867126bf6d7de737e6d658a
ADigitalNative <adigitalnative@gmail.com>
2012-06-28 14:55:11 -0400

    Converted nav to bootstrap-friendly & items to li items

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,27 +10,26 @@ module ApplicationHelper
 
   def session_controls
     if current_user
-      link_to("Sign Out", session_path, method: :delete).html_safe
+      content_tag(:li, link_to("Sign Out", session_path, method: :delete)).html_safe
     else
-      link_to("Sign In", login_path)
+      content_tag(:li, link_to("Sign In", login_path)).html_safe
     end
   end
 
   def instruction_links
     if current_user
-      [link_to("My Instructions", instructions_path),
-      link_to("New Instructions", new_instruction_path)].join(" ").html_safe
+      [content_tag(:li, link_to("My Instructions", instructions_path)),
+        content_tag(:li, link_to("New Instructions", new_instruction_path))].join("").html_safe
     end
   end
 
   def report_links
-    
     if current_user
-      links = [link_to("My Reports", reports_path)]
+      links = [content_tag(:li, link_to("My Reports", reports_path))]
       if current_user.latest_report
-        links << link_to("My Latest Report", current_user.latest_report)
+        links << content_tag(:li, link_to("My Latest Report", current_user.latest_report))
       end
-      links.join(" ").html_safe
+      links.join("").html_safe
     end
   end
 
