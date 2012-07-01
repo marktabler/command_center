45d1348227ac0a5f12978c16fc46359ba12da24a
Mark Tabler <mark.tabler@fallingmanstudios.net>
2012-07-01 19:13:44 -0400

    Fix alert creation bug

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -5,6 +5,7 @@ class AlertsController < ApplicationController
     if params[:pw] == INTERNAL_PASSWORD
       @instruction = Instruction.find(params["alert"]["instruction_id"])
       @instruction.create_alert(params["alert"]["body"])
+      return render nothing: true, status: 200
     else
       return render nothing: true, status: 403
     end
